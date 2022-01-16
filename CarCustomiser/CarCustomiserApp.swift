11dd523e632efac8c1d95987903ef04435e9dfa6
HarrySibson <94445782+HarrySibson@users.noreply.github.com>
2022-01-16 16:30:39 +0000

    Initial Commit

---
+++ CarCustomiser/CarCustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomiserApp.swift
+//  CarCustomiser
+//
+//  Created by Harry Sibson on 16/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
