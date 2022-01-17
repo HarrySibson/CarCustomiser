b0941d5e08ac5ae2c3de0a48bf7a904260a70d5e
HarrySibson <94445782+HarrySibson@users.noreply.github.com>
2022-01-17 18:49:51 +0000

    Started on Car Customiser Project

---
+++ CarCustomiser/Car.swift
@@ -0,0 +1,17 @@
+//
+//  Car.swift
+//  CarCustomiser
+//
+//  Created by Harry Sibson on 17/01/2022.
+//
+
+import Foundation
+struct Car {
+    let make: String
+    let model: String
+    var topSpeed: Int
+    var acceleration: Double
+    var handling: Int
+
+    
+}
--- CarCustomiser/ContentView.swift
+++ CarCustomiser/ContentView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    let car = Car(make: "Mazda", model:"MX-5", topSpeed: 125, acceleration: 7.7, handling: 5)
+    
     var body: some View {
-        Text("Hello, world!")
+        Text("Car Make: \(car.make)")
+        Text("Car Model: \(car.model)")
+        Text("Car Top Speed: \(car.topSpeed)mph")
+        Text("Car Acceleration (0-60): \(car.acceleration)s")
+        Text("Car Handling: \(car.handling)")
             .padding()
     }
 }
